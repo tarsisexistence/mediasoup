b720a88da3453dc69f5404ebd78b21589a89693a
Max Tarsis <21989873+untaggable@users.noreply.github.com>
2021-11-06 14:52:48 +0200

    refactor: move svc frontend code from echo example into standalone svc example

--- rust/examples-frontend/echo/src/index.ts
+++ rust/examples-frontend/echo/src/index.ts
@@ -2,7 +2,6 @@
 import { Device } from 'mediasoup-client';
 import { MediaKind, RtpCapabilities, RtpParameters } from 'mediasoup-client/lib/RtpParameters';
 import { DtlsParameters, TransportOptions, Transport } from 'mediasoup-client/lib/Transport';
-import { Producer } from 'mediasoup-client/lib/types';
 import { ConsumerOptions } from 'mediasoup-client/lib/Consumer';
 
 type Brand<K, T> = K & { __brand: T };
@@ -11,10 +10,10 @@ type ConsumerId = Brand<string, 'ConsumerId'>;
 type ProducerId = Brand<string, 'ProducerId'>;
 
 interface ServerInit {
-    action: 'Init';
-    consumerTransportOptions: TransportOptions;
-    producerTransportOptions: TransportOptions;
-    routerRtpCapabilities: RtpCapabilities;
+	action: 'Init';
+	consumerTransportOptions: TransportOptions;
+	producerTransportOptions: TransportOptions;
+	routerRtpCapabilities: RtpCapabilities;
 }
 
 interface ServerConnectedProducerTransport {
@@ -45,34 +44,34 @@ type ServerMessage =
 	ServerConsumed;
 
 interface ClientInit {
-    action: 'Init';
-    rtpCapabilities: RtpCapabilities;
+	action: 'Init';
+	rtpCapabilities: RtpCapabilities;
 }
 
 interface ClientConnectProducerTransport {
-    action: 'ConnectProducerTransport';
+	action: 'ConnectProducerTransport';
 	dtlsParameters: DtlsParameters;
 }
 
 interface ClientConnectConsumerTransport {
-    action: 'ConnectConsumerTransport';
-    dtlsParameters: DtlsParameters;
+	action: 'ConnectConsumerTransport';
+	dtlsParameters: DtlsParameters;
 }
 
 interface ClientProduce {
-    action: 'Produce';
-    kind: MediaKind;
-    rtpParameters: RtpParameters;
+	action: 'Produce';
+	kind: MediaKind;
+	rtpParameters: RtpParameters;
 }
 
 interface ClientConsume {
-    action: 'Consume';
-    producerId: ProducerId;
+	action: 'Consume';
+	producerId: ProducerId;
 }
 
 interface ClientConsumerResume {
-    action: 'ConsumerResume';
-    id: ConsumerId;
+	action: 'ConsumerResume';
+	id: ConsumerId;
 }
 
 type ClientMessage =
@@ -88,37 +87,14 @@ async function init()
 	const sendPreview = document.querySelector('#preview-send') as HTMLVideoElement;
 	const receivePreview = document.querySelector('#preview-receive') as HTMLVideoElement;
 
-	sendPreview.onloadedmetadata = () => sendPreview.play();
-	receivePreview.onloadedmetadata = () => receivePreview.play();
-
-	const SL = document.querySelector('#SL') as HTMLSpanElement
-	const decSL = document.querySelector('#decSL') as HTMLButtonElement;
-	const incSL = document.querySelector('#incSL') as HTMLButtonElement;
-
-	let videoProducer: Producer | null = null;
-	let maxSpatialLayer = -1
-
-	decSL.addEventListener('click', () => {
-		if (maxSpatialLayer > -1) {
-			maxSpatialLayer -=1
-			renderMaxSpatialLayer()
-		}
-	});
-	incSL.addEventListener('click', () => {
-		if (videoProducer?.rtpParameters.encodings && maxSpatialLayer < videoProducer.rtpParameters.encodings.length - 1) {
-			maxSpatialLayer += 1
-			renderMaxSpatialLayer()
-		}
-	});
-
-
-	const renderMaxSpatialLayer = () => {
-		SL.innerHTML = maxSpatialLayer > - 1 ? String(maxSpatialLayer) : 'none';
-
-		videoProducer?.setMaxSpatialLayer(maxSpatialLayer)
-	}
-
-	renderMaxSpatialLayer()
+	sendPreview.onloadedmetadata = () =>
+	{
+		sendPreview.play();
+	};
+	receivePreview.onloadedmetadata = () =>
+	{
+		receivePreview.play();
+	};
 
 	const receiveMediaStream = new MediaStream();
 
@@ -133,194 +109,179 @@ async function init()
 	let producerTransport: Transport | undefined;
 	let consumerTransport: Transport | undefined;
 
-	const waitingForResponse: Map<ServerMessage['action'], Function> = new Map();
-
-	ws.onmessage = async (message) =>
 	{
-		const decodedMessage: ServerMessage = JSON.parse(message.data);
+		const waitingForResponse: Map<ServerMessage['action'], Function> = new Map();
 
-		switch (decodedMessage.action)
+		ws.onmessage = async (message) =>
 		{
-			case 'Init': {
-				// It is expected that server will send initialization message right after
-				// WebSocket connection is established
-				await device.load({
-					routerRtpCapabilities : decodedMessage.routerRtpCapabilities
-				});
+			const decodedMessage: ServerMessage = JSON.parse(message.data);
 
-				// Send client-side initialization message back right away
-				send({
-					action          : 'Init',
-					rtpCapabilities : device.rtpCapabilities
-				});
-
-				// Producer transport is needed to send audio and video to SFU
-				producerTransport = device.createSendTransport(
-					decodedMessage.producerTransportOptions
-				);
-
-				producerTransport
-					.on('connect', ({ dtlsParameters }, success) =>
-					{
-						// Send request to establish producer transport connection
-						send({
-							action : 'ConnectProducerTransport',
-							dtlsParameters
-						});
-						// And wait for confirmation, but, obviously, no error handling,
-						// which you should definitely have in real-world applications
-						waitingForResponse.set('ConnectedProducerTransport', () =>
-						{
-							success();
-							console.log('Producer transport connected');
-						});
-					})
-					.on('produce', ({ kind, rtpParameters }, success) =>
-					{
-						// Once connection is established, send request to produce
-						// audio or video track
-						send({
-							action : 'Produce',
-							kind,
-							rtpParameters,
-						});
-						// And wait for confirmation, but, obviously, no error handling,
-						// which you should definitely have in real-world applications
-						waitingForResponse.set('Produced', ({ id }: {id: string}) =>
-						{
-							success({ id });
-						});
+			switch (decodedMessage.action)
+			{
+				case 'Init': {
+					// It is expected that server will send initialization message right after
+					// WebSocket connection is established
+					await device.load({
+						routerRtpCapabilities : decodedMessage.routerRtpCapabilities
 					});
 
-				// Request microphone and camera access, in real-world apps you may want
-				// to do this separately so that audio-only and video-only cases are
-				// handled nicely instead of failing completely
-				const mediaStream = await navigator.mediaDevices.getUserMedia({
-					audio : true,
-					video : {
-						width : {
-							ideal : 1280
-						},
-						height : {
-							ideal : 720
-						},
-						frameRate : {
-							ideal : 60
-						}
-					}
-				});
-
-				sendPreview.srcObject = mediaStream;
-
-				const producers = [];
-
-				// And create producers for all tracks that were previously requested
-				for (const track of mediaStream.getTracks())
-				{
-					const producer = await producerTransport.produce({
-						track,
-						encodings: track.kind === 'video' ? [
-							{ maxBitrate: 100000 },
-							{ maxBitrate: 300000 },
-							{ maxBitrate: 900000 },
-						] : undefined
+					// Send client-side initialization message back right away
+					send({
+						action          : 'Init',
+						rtpCapabilities : device.rtpCapabilities
 					});
 
-					producers.push(producer);
-					console.log(`${track.kind} producer created:`, producer);
+					// Producer transport is needed to send audio and video to SFU
+					producerTransport = device.createSendTransport(
+						decodedMessage.producerTransportOptions
+					);
 
-					if (producer.kind === 'video') {
-						videoProducer = producer;
-						maxSpatialLayer = videoProducer.rtpParameters.encodings ?  videoProducer.rtpParameters.encodings.length - 1 : -1
-						renderMaxSpatialLayer()
-					}
-				}
-
-				// Consumer transport is now needed to receive previously produced
-				// tracks back
-				consumerTransport = device.createRecvTransport(
-					decodedMessage.consumerTransportOptions
-				);
-
-				consumerTransport
-					.on('connect', ({ dtlsParameters }, success) =>
-					{
-						// Send request to establish consumer transport connection
-						send({
-							action : 'ConnectConsumerTransport',
-							dtlsParameters
-						});
-						// And wait for confirmation, but, obviously, no error handling,
-						// which you should definitely have in real-world applications
-						waitingForResponse.set('ConnectedConsumerTransport', () =>
+					producerTransport
+						.on('connect', ({ dtlsParameters }, success) =>
 						{
-							success();
-							console.log('Consumer transport connected');
-						});
-					});
-
-				// For simplicity of this example producers were stored in an array
-				// and are now all consumed one at a time
-				for (const producer of producers)
-				{
-					await new Promise((resolve) =>
-					{
-						// Send request to consume producer
-						send({
-							action     : 'Consume',
-							producerId : producer.id as ProducerId
-						});
-						// And wait for confirmation, but, obviously, no error handling,
-						// which you should definitely have in real-world applications
-						waitingForResponse.set('Consumed', async (consumerOptions: ConsumerOptions) =>
-						{
-							// Once confirmation is received, corresponding consumer
-							// can be created client-side
-							const consumer = await (consumerTransport as Transport).consume(
-								consumerOptions
-							);
-
-							console.log(`${consumer.kind} consumer created:`, consumer);
-
-							// Consumer needs to be resumed after being created in
-							// paused state (see official documentation about why:
-							// https://mediasoup.org/documentation/v3/mediasoup/api/#transport-consume)
+							// Send request to establish producer transport connection
 							send({
-								action : 'ConsumerResume',
-								id     : consumer.id as ConsumerId
+								action : 'ConnectProducerTransport',
+								dtlsParameters
 							});
-
-							receiveMediaStream.addTrack(consumer.track);
-							receivePreview.srcObject = receiveMediaStream;
-
-							if (consumer.kind === 'video') {
-								// consumerVideo = consumer.rtpParameters
-							}
-
-							resolve(undefined);
+							// And wait for confirmation, but, obviously, no error handling,
+							// which you should definitely have in real-world applications
+							waitingForResponse.set('ConnectedProducerTransport', () =>
+							{
+								success();
+								console.log('Producer transport connected');
+							});
+						})
+						.on('produce', ({ kind, rtpParameters }, success) =>
+						{
+							// Once connection is established, send request to produce
+							// audio or video track
+							send({
+								action : 'Produce',
+								kind,
+								rtpParameters
+							});
+							// And wait for confirmation, but, obviously, no error handling,
+							// which you should definitely have in real-world applications
+							waitingForResponse.set('Produced', ({ id }: {id: string}) =>
+							{
+								success({ id });
+							});
 						});
+
+					// Request microphone and camera access, in real-world apps you may want
+					// to do this separately so that audio-only and video-only cases are
+					// handled nicely instead of failing completely
+					const mediaStream = await navigator.mediaDevices.getUserMedia({
+						audio : true,
+						video : {
+							width : {
+								ideal : 1270
+							},
+							height : {
+								ideal : 720
+							},
+							frameRate : {
+								ideal : 60
+							}
+						}
 					});
-				}
 
-				break;
-			}
-			default: {
-				// All messages other than initialization go here and are assumed
-				// to be notifications that correspond to previously sent requests
-				const callback = waitingForResponse.get(decodedMessage.action);
+					sendPreview.srcObject = mediaStream;
 
-				if (callback)
-				{
-					waitingForResponse.delete(decodedMessage.action);
-					callback(decodedMessage);
+					const producers = [];
+
+					// And create producers for all tracks that were previously requested
+					for (const track of mediaStream.getTracks())
+					{
+						const producer = await producerTransport.produce({ track });
+
+						producers.push(producer);
+						console.log(`${track.kind} producer created:`, producer);
+					}
+
+					// Consumer transport is now needed to receive previously produced
+					// tracks back
+					consumerTransport = device.createRecvTransport(
+						decodedMessage.consumerTransportOptions
+					);
+
+					consumerTransport
+						.on('connect', ({ dtlsParameters }, success) =>
+						{
+							// Send request to establish consumer transport connection
+							send({
+								action : 'ConnectConsumerTransport',
+								dtlsParameters
+							});
+							// And wait for confirmation, but, obviously, no error handling,
+							// which you should definitely have in real-world applications
+							waitingForResponse.set('ConnectedConsumerTransport', () =>
+							{
+								success();
+								console.log('Consumer transport connected');
+							});
+						});
+
+					// For simplicity of this example producers were stored in an array
+					// and are now all consumed one at a time
+					for (const producer of producers)
+					{
+						await new Promise((resolve) =>
+						{
+							// Send request to consume producer
+							send({
+								action     : 'Consume',
+								producerId : producer.id as ProducerId
+							});
+							// And wait for confirmation, but, obviously, no error handling,
+							// which you should definitely have in real-world applications
+							waitingForResponse.set('Consumed', async (consumerOptions: ConsumerOptions) =>
+							{
+								// Once confirmation is received, corresponding consumer
+								// can be created client-side
+								const consumer = await (consumerTransport as Transport).consume(
+									consumerOptions
+								);
+
+								console.log(`${consumer.kind} consumer created:`, consumer);
+
+								// Consumer needs to be resumed after being created in
+								// paused state (see official documentation about why:
+								// https://mediasoup.org/documentation/v3/mediasoup/api/#transport-consume)
+								send({
+									action : 'ConsumerResume',
+									id     : consumer.id as ConsumerId
+								});
+
+								receiveMediaStream.addTrack(consumer.track);
+								receivePreview.srcObject = receiveMediaStream;
+
+								resolve(undefined);
+							});
+						});
+					}
+
+					break;
 				}
-				else
-				{
-					console.error('Received unexpected message', decodedMessage);
+				default: {
+					// All messages other than initialization go here and are assumed
+					// to be notifications that correspond to previously sent requests
+					const callback = waitingForResponse.get(decodedMessage.action);
+
+					if (callback)
+					{
+						waitingForResponse.delete(decodedMessage.action);
+						callback(decodedMessage);
+					}
+					else
+					{
+						console.error('Received unexpected message', decodedMessage);
+					}
 				}
 			}
-		}
-	};
+		};
+	}
 	ws.onerror = console.error;
 }
 
--- rust/examples-frontend/svc/src/index.ts
+++ rust/examples-frontend/svc/src/index.ts
@@ -1,6 +1,8 @@
+/* eslint-disable no-console */
 import { Device } from 'mediasoup-client';
 import { MediaKind, RtpCapabilities, RtpParameters } from 'mediasoup-client/lib/RtpParameters';
 import { DtlsParameters, TransportOptions, Transport } from 'mediasoup-client/lib/Transport';
+import { Producer } from 'mediasoup-client/lib/types';
 import { ConsumerOptions } from 'mediasoup-client/lib/Consumer';
 
 type Brand<K, T> = K & { __brand: T };
@@ -81,17 +83,51 @@ type ClientMessage =
     ClientConsume |
     ClientConsumerResume;
 
-async function init() {
-    const sendPreviewNode = document.querySelector('#preview-send') as HTMLVideoElement;
-    const receivePreviewNode = document.querySelector('#preview-receive') as HTMLVideoElement;
+async function init()
+{
+    const sendPreview = document.querySelector('#preview-send') as HTMLVideoElement;
+    const receivePreview = document.querySelector('#preview-receive') as HTMLVideoElement;
 
-    sendPreviewNode.onloadedmetadata = () => sendPreviewNode.play();
-    receivePreviewNode.onloadedmetadata = () => receivePreviewNode.play();
+    sendPreview.onloadedmetadata = () => sendPreview.play();
+    receivePreview.onloadedmetadata = () => receivePreview.play();
+
+    const SL = document.querySelector('#SL') as HTMLSpanElement
+    const decSL = document.querySelector('#decSL') as HTMLButtonElement;
+    const incSL = document.querySelector('#incSL') as HTMLButtonElement;
+
+    let videoProducer: Producer | null = null;
+    let maxSpatialLayer = -1
+
+    decSL.addEventListener('click', () => {
+        if (maxSpatialLayer > -1) {
+            maxSpatialLayer -=1
+            renderMaxSpatialLayer()
+        }
+    });
+    incSL.addEventListener('click', () => {
+        if (videoProducer?.rtpParameters.encodings && maxSpatialLayer < videoProducer.rtpParameters.encodings.length - 1) {
+            maxSpatialLayer += 1
+            renderMaxSpatialLayer()
+        }
+    });
+
+
+    const renderMaxSpatialLayer = () => {
+        SL.innerHTML = maxSpatialLayer > - 1 ? String(maxSpatialLayer) : 'none';
+
+        videoProducer?.setMaxSpatialLayer(maxSpatialLayer)
+    }
+
+    renderMaxSpatialLayer()
 
     const receiveMediaStream = new MediaStream();
 
     const ws = new WebSocket('ws://localhost:3000/ws');
-    const send = (message: ClientMessage) => ws.send(JSON.stringify(message));
+
+    function send(message: ClientMessage)
+    {
+        ws.send(JSON.stringify(message));
+    }
 
     const device = new Device();
     let producerTransport: Transport | undefined;
@@ -99,10 +135,12 @@ async function init() {
 
     const waitingForResponse: Map<ServerMessage['action'], Function> = new Map();
 
-    ws.onmessage = async (message) => {
+    ws.onmessage = async (message) =>
+    {
         const decodedMessage: ServerMessage = JSON.parse(message.data);
 
-        switch (decodedMessage.action) {
+        switch (decodedMessage.action)
+        {
             case 'Init': {
                 // It is expected that server will send initialization message right after
                 // WebSocket connection is established
@@ -122,34 +160,35 @@ async function init() {
                 );
 
                 producerTransport
-                    .on('connect', ({ dtlsParameters }, success) => {
-                    // Send request to establish producer transport connection
-                    send({
-                        action : 'ConnectProducerTransport',
-                        dtlsParameters
-                    });
-
-                    // And wait for confirmation, but, obviously, no error handling,
-                    // which you should definitely have in real-world applications
-                    waitingForResponse.set('ConnectedProducerTransport', () => {
-                        success();
-                        console.log('Producer transport connected');
-                    });
-                })
-                    .on('produce', ({ kind, rtpParameters }, success) => {
+                    .on('connect', ({ dtlsParameters }, success) =>
+                    {
+                        // Send request to establish producer transport connection
+                        send({
+                            action : 'ConnectProducerTransport',
+                            dtlsParameters
+                        });
+                        // And wait for confirmation, but, obviously, no error handling,
+                        // which you should definitely have in real-world applications
+                        waitingForResponse.set('ConnectedProducerTransport', () =>
+                        {
+                            success();
+                            console.log('Producer transport connected');
+                        });
+                    })
+                    .on('produce', ({ kind, rtpParameters }, success) =>
+                    {
                         // Once connection is established, send request to produce
                         // audio or video track
                         send({
                             action : 'Produce',
                             kind,
-                            rtpParameters
+                            rtpParameters,
                         });
-
                         // And wait for confirmation, but, obviously, no error handling,
                         // which you should definitely have in real-world applications
-                        waitingForResponse.set('Produced', ({ id }: { id: string }) => {
+                        waitingForResponse.set('Produced', ({ id }: {id: string}) =>
+                        {
                             success({ id });
-                            console.log('Produced successfully!');
                         });
                     });
 
@@ -171,50 +210,76 @@ async function init() {
                     }
                 });
 
-                sendPreviewNode.srcObject = mediaStream;
+                sendPreview.srcObject = mediaStream;
 
                 const producers = [];
 
                 // And create producers for all tracks that were previously requested
-                for (const track of mediaStream.getTracks()) {
-                    const producer = await producerTransport.produce({ track });
+                for (const track of mediaStream.getTracks())
+                {
+                    const producer = await producerTransport.produce({
+                        track,
+                        encodings: track.kind === 'video' ? [
+                            { maxBitrate: 100000 },
+                            { maxBitrate: 300000 },
+                            { maxBitrate: 900000 },
+                        ] : undefined
+                    });
+
                     producers.push(producer);
                     console.log(`${track.kind} producer created:`, producer);
+
+                    if (producer.kind === 'video') {
+                        videoProducer = producer;
+                        maxSpatialLayer = videoProducer.rtpParameters.encodings ?  videoProducer.rtpParameters.encodings.length - 1 : -1
+                        renderMaxSpatialLayer()
+                    }
                 }
 
-                // Consumer transport is now needed to receive previously produced tracks back
-                consumerTransport = device.createRecvTransport(decodedMessage.consumerTransportOptions);
+                // Consumer transport is now needed to receive previously produced
+                // tracks back
+                consumerTransport = device.createRecvTransport(
+                    decodedMessage.consumerTransportOptions
+                );
 
-                consumerTransport.on('connect', ({ dtlsParameters }, success) => {
-                    // Send request to establish consumer transport connection
-                    send({
-                        action : 'ConnectConsumerTransport',
-                        dtlsParameters
+                consumerTransport
+                    .on('connect', ({ dtlsParameters }, success) =>
+                    {
+                        // Send request to establish consumer transport connection
+                        send({
+                            action : 'ConnectConsumerTransport',
+                            dtlsParameters
+                        });
+                        // And wait for confirmation, but, obviously, no error handling,
+                        // which you should definitely have in real-world applications
+                        waitingForResponse.set('ConnectedConsumerTransport', () =>
+                        {
+                            success();
+                            console.log('Consumer transport connected');
+                        });
                     });
-
-                    // And wait for confirmation, but, obviously, no error handling,
-                    // which you should definitely have in real-world applications
-                    waitingForResponse.set('ConnectedConsumerTransport', () => {
-                        success();
-                        console.log('Consumer transport connected');
-                    });
-                });
 
                 // For simplicity of this example producers were stored in an array
                 // and are now all consumed one at a time
-                for (const producer of producers) {
-                    await new Promise((resolve) => {
+                for (const producer of producers)
+                {
+                    await new Promise((resolve) =>
+                    {
                         // Send request to consume producer
                         send({
                             action     : 'Consume',
                             producerId : producer.id as ProducerId
                         });
-
                         // And wait for confirmation, but, obviously, no error handling,
                         // which you should definitely have in real-world applications
-                        waitingForResponse.set('Consumed', async (consumerOptions: ConsumerOptions) => {
-                            // Once confirmation is received, corresponding consumer can be created client-side
-                            const consumer = await (consumerTransport as Transport).consume(consumerOptions);
+                        waitingForResponse.set('Consumed', async (consumerOptions: ConsumerOptions) =>
+                        {
+                            // Once confirmation is received, corresponding consumer
+                            // can be created client-side
+                            const consumer = await (consumerTransport as Transport).consume(
+                                consumerOptions
+                            );
+
                             console.log(`${consumer.kind} consumer created:`, consumer);
 
                             // Consumer needs to be resumed after being created in
@@ -226,30 +291,37 @@ async function init() {
                             });
 
                             receiveMediaStream.addTrack(consumer.track);
-                            receivePreviewNode.srcObject = receiveMediaStream;
+                            receivePreview.srcObject = receiveMediaStream;
+
+                            if (consumer.kind === 'video') {
+                                // consumerVideo = consumer.rtpParameters
+                            }
 
                             resolve(undefined);
                         });
                     });
                 }
 
-                break
+                break;
             }
-
             default: {
                 // All messages other than initialization go here and are assumed
                 // to be notifications that correspond to previously sent requests
                 const callback = waitingForResponse.get(decodedMessage.action);
 
-                if (callback) {
+                if (callback)
+                {
                     waitingForResponse.delete(decodedMessage.action);
                     callback(decodedMessage);
-                } else {
+                }
+                else
+                {
                     console.error('Received unexpected message', decodedMessage);
                 }
             }
         }
-    }
+    };
+    ws.onerror = console.error;
 }
 
-init()
+init();
